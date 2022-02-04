d40f1cefc4ea8d0b7b1811a4805ac8c6fd543e2f
Joris Conijn <jorisconijn@binx.io>
2022-02-04 20:51:56 +0100

    feat: include the cfn-guard loading error
    When cfn-guard is not capable of running a test. For example when the test yaml has an invalid format. We include the append the error message to the message fed to the wrapper. This will give additional context on what went wrong.

--- cfn_guard_test/reader.py
+++ cfn_guard_test/reader.py
@@ -34,7 +34,8 @@ class CfnGuardReader:
 
         if not self.__valid_report:
             case = ErrorTestCase(
-                message=f"Unable to read the output when testing '{rule_name}'!"
+                message=f"Unable to read the output when testing '{rule_name}'!",
+                details=report.decode("utf-8"),
             )
             self.__suite.add_test_case(case)
 
